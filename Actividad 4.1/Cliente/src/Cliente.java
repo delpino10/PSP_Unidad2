d5dbf0ada2d0a61859e555d31ba764481af55423
Jose Antonio del Pino <jadelpino10@gmail.com>
2024-11-27 11:03:27 +0200

    Actividad 4.1 corregida

--- Actividad 4.1/Cliente/src/Cliente.java
+++ Actividad 4.1/Cliente/src/Cliente.java
@@ -8,15 +8,15 @@ import java.net.UnknownHostException;
 
 public class Cliente {
     public static void main(String[] args) throws UnknownHostException {
-        if (args.length != 1) {
+        if (args.length != 2) {
             System.out.println("Por favor, ingrese la dirección IP y el puerto.");
             return;
         }
         String ip = args[0];   // Dirección IP del servidor
-        InetAddress inetAddress = InetAddress.getLocalHost();
+        InetAddress direccionIp = InetAddress.getByName(ip);
         int puerto = Integer.parseInt(args[0]);  // Puerto del servidor
         try (
-                Socket socket = new Socket(inetAddress, puerto);
+                Socket socket = new Socket(direccionIp, puerto);
                 PrintWriter socketOut = new PrintWriter(socket.getOutputStream(), true);
                 BufferedReader socketIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))
--- Actividad 4.1/Servidor/src/Servidor.java
+++ Actividad 4.1/Servidor/src/Servidor.java
@@ -11,34 +11,50 @@ public class Servidor {
             System.err.println("Por favor, ingrese el número de puerto.");
             System.exit(1);
         }
-        int puerto = Integer.parseInt(args[0]);  // Obtenemos el puerto desde los parámetros de la línea de comando
-//        int puerto = 8000;  // Obtenemos el puerto desde los parámetros de la línea de comando
+
+        int puerto = 0;  // Obtenemos el puerto desde los parámetros de la línea de comando (args[0])
+        try {
+            puerto = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            System.out.printf("El puerto debe ser un número [puerto = %s]", args[0]);
+            System.exit(1);
+        }
+
         System.out.println("Servidor escuchando en el puerto " + puerto);
-        // Aquí se incluiría la lógica para iniciar el servidor (por ejemplo, un servidor de sockets)
+
+        // A partir de aquí se incluiría la lógica para iniciar el servidor
         try (
+                // Crea un socket de tipo servidor (ServerSocket en Java)
                 ServerSocket servidor = new ServerSocket(puerto);
+                // El serivdor establece la conexión con el cliente
+                // Si queremos más conexiones de más clientes, crear más socket cliente
                 Socket cliente = servidor.accept();
-                PrintWriter salida = new PrintWriter(cliente.getOutputStream(), true);
-                BufferedReader entrada = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+                // InputStream del cliente al Servidor
+                BufferedReader flujoEntrada = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+                // OutputStream del servidor al cliente
+                PrintWriter flujoSalida = new PrintWriter(cliente.getOutputStream(), true);
         ) {
             String mensaje;
-            while ((mensaje = entrada.readLine()) != null) {
-                //salida.println(mensaje);
+            while ((mensaje = flujoEntrada.readLine()) != null) {
+                // Salida del texto por consola
                 System.out.println(mensaje);
+                // Salida del texto por pantalla del cliente
+                flujoSalida.println(mensaje);
                 // Terminamos la conexión si escribimos "fin"
                 if(mensaje.equals("fin")){
                     // Mensaje que aparece en la consola
                     System.out.println("Palabra clave recibida. Cerrando servidor...");
                     // Mensaje que aparece en el cliente
-                    salida.println("Servidor cerrado. Adiós.");
+                    flujoSalida.println("Servidor cerrado. Adios.");
                     break;
                 }else{
-                    //
-                    salida.println(mensaje);
+                    // Mensaje escrito por pantalla
+                    flujoSalida.println(mensaje);
                 }
             }
         }catch (IOException e) {
             System.out.println("Error al escuchar el puerto " + puerto);
+            System.err.println(e.getMessage());
         }
     }
 }
